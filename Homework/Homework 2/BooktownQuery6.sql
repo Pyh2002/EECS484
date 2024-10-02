c1791643947015911535085eeea809a4b0d8c16a
the_great_tomato <pyh180886@126.com>
2024-10-02 17:37:52 -0400

    refactor: code refactor

--- Homework/Homework 2/BooktownQuery6.sql
+++ Homework/Homework 2/BooktownQuery6.sql
@@ -12,12 +12,17 @@ WHERE
     b.Book_ID = e.Book_ID
     AND b.Author_ID = a.Author_ID
     AND a.Author_ID IN(
-        SELECT a.Author_ID
-        FROM Authors a, Books b, Editions e
-        WHERE e.Book_ID = b.Book_ID
-        AND a.Author_ID = b.Author_ID
-        AND e.Publication_Date >= '2003-01-01'
-        AND e.Publication_Date <= '2008-12-31'
+        SELECT
+            a.Author_ID
+        FROM
+            Authors a,
+            Books b,
+            Editions e
+        WHERE
+            e.Book_ID = b.Book_ID
+            AND a.Author_ID = b.Author_ID
+            AND e.Publication_Date >= '2003-01-01'
+            AND e.Publication_Date <= '2008-12-31'
     )
 ORDER BY
     a.Author_ID ASC,
