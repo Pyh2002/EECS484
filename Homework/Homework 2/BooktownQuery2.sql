0e0200e55479c8a20757724eb7b30bc3c0206e16
the_great_tomato <pyh180886@126.com>
2024-10-28 14:54:23 -0400

    docs(slides): slides uploaded and midterm uploaded

--- Homework/Homework 2/BooktownQuery2.sql
+++ Homework/Homework 2/BooktownQuery2.sql
@@ -1,18 +1,26 @@
-SELECT
-    Subject
-FROM
-    (
-        SELECT
-            DISTINCT s1.Subject
-        FROM
-            Subjects s1
-        MINUS
-        SELECT
-            DISTINCT s2.Subject
-        FROM
-            Subjects s2
-            JOIN Books b ON s2.Subject_ID = b.Subject_ID
-            JOIN Authors a ON b.Author_ID = a.Author_ID
-    ) result
-ORDER BY
-    Subject ASC;
+-- SELECT
+--     Subject
+-- FROM
+--     (
+--         SELECT
+--             DISTINCT s1.Subject
+--         FROM
+--             Subjects s1
+--         MINUS
+--         SELECT
+--             DISTINCT s2.Subject
+--         FROM
+--             Subjects s2
+--             JOIN Books b ON s2.Subject_ID = b.Subject_ID
+--             JOIN Authors a ON b.Author_ID = a.Author_ID
+--     ) result
+-- ORDER BY
+--     Subject ASC;
+
+SELECT S.SUbject
+FROM Subjects S
+MINUS
+SELECT S.Subject
+FROM Subjects S
+JOIN Books B ON S.Subject_ID = B.Subject_ID
+ORDER BY S.Subject ASC;
